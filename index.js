9c969ba4784db2c671096a4d5143b5304ed0ad0c
Andy Dayton <amdayton@gmail.com>
2015-11-14 21:23:41 -0500

    Add config files and index file

--- index.js
+++ index.js
@@ -1,29 +1,63 @@
-var path = require('path');
-var fs = require('fs');
-var omx = require('omxdirector').enableNativeLoop();;
+// require libs
+var path = require('path')
+  , fs = require('fs')
+  , omx = require('omxdirector').enableNativeLoop()
+  , RFID = require('./rfid.js');
 
-var p = '/media/sfpc-media/share/';
-var playlist = [];
+// config files
+var pkg = require('./package.json')
+  , config = require('./config.json');
+
+var rfid;
+
+
+function init() {
+  printIntro();
+
+  rfid = new RFID();
+  rfid.on('change', onTagChange);
+  rfid.start();
+
+}
+
+
+function onTagChange(id) {
+  console.log("Change tag: "+ id);
+
+  if(config.playlistMapping[id]) {
+    console.log("Set play directory: "+config.mediaDir+config.playlistMapping[id]);    
+  }
+}
+ 
+function printIntro() {
+  console.log(pkg.title+' v'+pkg.version);
+}
 
 // read all files in given directory
-fs.readdir(p, function(err, files) {
-  if(err) {
-    throw err;
-  }
+// fs.readdir(p, function(err, files) {
+//   if(err) {
+//     throw err;
+//   }
 
-  files.map(function (file) {
-    // get full file path
-    return path.join(p, file);
-  }).filter(function (file) {
-    // filter only playable movie files
-    var re = /\.(mov|avi|mp4)$/i;
-    return fs.statSync(file).isFile() && re.test(file);
-  }).forEach(function (file) {
-    playlist.push(file);
-  });
+//   files.map(function (file) {
+//     // get full file path
+//     return path.join(p, file);
+//   }).filter(function (file) {
+//     // filter only playable movie files
+//     var re = /\.(mov|avi|mp4)$/i;
+//     return fs.statSync(file).isFile() && re.test(file);
+//   }).forEach(function (file) {
+//     playlist.push(file);
+//   });
 
-  console.log('playlist: ', playlist);
+//   console.log('playlist: ', playlist);
 
-  omx.play(playlist, {loop: true});
+//   omx.play(playlist, {loop: true});
 
-});
+// });
+
+
+// jenky way of keeping script open
+// 
+init();
+// setInterval(function(){}, 1000);
